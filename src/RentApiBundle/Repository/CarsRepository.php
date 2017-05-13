7089c1ec8a14a2432f8465bc7885299c8504f308
Luca Vlad <luca.vlad08@gmail.com>
2017-05-13 14:47:26 +0300

    Small fix.

--- src/RentApiBundle/Repository/CarsRepository.php
+++ src/RentApiBundle/Repository/CarsRepository.php
@@ -12,21 +12,21 @@ use Doctrine\ORM\EntityRepository;
  */
 class CarsRepository extends EntityRepository
 {
-    const DEFAULT_CATEGORIES = array(
+    private static $DEFAULT_CATEGORIES = array(
         'urban',
         'compact',
         'monospace',
         'utilities'
     );
 
-    const DEFAULT_FUEL_TYPES = array(
+    private static $DEFAULT_FUEL_TYPES = array(
         'Petrol',
         'Diesel',
         'Hybrid',
         'Electric'
     );
 
-    const DEFAULT_TRANSMISSION_TYPES = array(
+    private static $DEFAULT_TRANSMISSION_TYPES = array(
         'Automatic',
         'Manual'
     );
@@ -44,9 +44,9 @@ class CarsRepository extends EntityRepository
             ->andWhere('car.transmissionType IN (:transmissionTypes)')
             ->andWhere('car.doorsNumber >= :doorsNumber')
             ->andWhere('car.seats >= :passengers')
-            ->setParameter('categories', $categories ? $categories : self::DEFAULT_CATEGORIES)
-            ->setParameter('fuelTypes', $fuelTypes ? $fuelTypes : self::DEFAULT_FUEL_TYPES)
-            ->setParameter('transmissionTypes', $transmissionTypes ? $transmissionTypes : self::DEFAULT_TRANSMISSION_TYPES)
+            ->setParameter('categories', $categories ? $categories : self::$DEFAULT_CATEGORIES)
+            ->setParameter('fuelTypes', $fuelTypes ? $fuelTypes : self::$DEFAULT_FUEL_TYPES)
+            ->setParameter('transmissionTypes', $transmissionTypes ? $transmissionTypes : self::$DEFAULT_TRANSMISSION_TYPES)
             ->setParameter('doorsNumber', $doorsNumber ? $doorsNumber : self::DEFAULT_DOORS_NUMBER)
             ->setParameter('passengers', $passengers ? $passengers : self::DEFAULT_PASSENGERS_NUMBER);
 
